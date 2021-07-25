57c55c0e3a45ad3066597a9079c18b1043472a12
Muhammad <elnasabdallah@gmail.com>
2021-07-25 15:35:36 +0200

    user  signin api

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -1,7 +1,10 @@
 import express from "express";
 import expressAsyncHandler from "express-async-handler";
+import bcrypt from "bcryptjs";
 import User from "../models/userModel.js";
 import data from "./../data.js";
+import { generateToken } from "../utils.js";
+
 const userRouter = express.Router();
 
 userRouter.get(
@@ -9,6 +12,26 @@ userRouter.get(
   expressAsyncHandler(async (req, res) => {
     const createdUsers = await User.insertMany(data.users);
     res.send({ createdUsers });
+  })
+);
+
+userRouter.post(
+  "/signin",
+  expressAsyncHandler(async (req, res) => {
+    const user = await User.findOne({ email: req.body.email });
+    if (user) {
+      if (bcrypt.compareSync(req.body.password, user.password)) {
+        res.send({
+          _id: user._id,
+          name: user.name,
+          email: user.email,
+          isAdmin: user.isAdmin,
+          token: generateToken(user),
+        });
+        return;
+      }
+    }
+    res.status(401).send({ message: "Invalid email or password" });
   })
 );
 
